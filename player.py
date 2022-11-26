47313b3dfa44851a1225fb5fbabfa6a1eea9919c
Crimson Jack <crmsnjck@gmail.com>
2022-11-26 18:10:07 +0100

    feat(game): Alternative keys for player movement added.

--- player.py
+++ player.py
@@ -8,7 +8,7 @@ class Player(CustomDrawSprite):
     def __init__(self, image, position, groups, speed, exit_points, obstacle_sprites,
                  collectable_sprites, enemy_sprites, game_state):
         super().__init__(groups)
-        self.image = pygame.transform.scale(image, (settings.TILE_SIZE * 0.9, settings.TILE_SIZE * 0.9))
+        self.image = pygame.transform.scale(image, (int(settings.TILE_SIZE * 0.9), int(settings.TILE_SIZE * 0.9)))
         self.rect = self.image.get_rect(topleft=position)
         self.hit_box = self.rect.inflate(game_helper.calculate_ratio(-5), 0)
 
@@ -36,16 +36,16 @@ class Player(CustomDrawSprite):
         # Read pressed key and specify movement direction
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
             self.direction.y = -1
-        elif keys[pygame.K_DOWN]:
+        elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
             self.direction.y = 1
         else:
             self.direction.y = 0
 
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             self.direction.x = 1
-        elif keys[pygame.K_LEFT]:
+        elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
             self.direction.x = -1
         else:
             self.direction.x = 0
