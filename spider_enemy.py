f6b69cc7beb5bcb9e9b7cc80685d5accbb75cc91
crimsonking <jacek.jarczak@gmail.com>
2023-02-08 18:37:58 +0100

    feat(game): Spider enemy movement fixed. Moving obstacles work with Spiders.

--- spider_enemy.py
+++ spider_enemy.py
@@ -4,14 +4,14 @@ from custom_draw_sprite import CustomDrawSprite
 
 
 class SpiderEnemy(CustomDrawSprite):
-    def __init__(self, image, position, groups, speed, net_length):
+    def __init__(self, image, position, groups, speed, net_length, moving_obstacle_sprites):
         super().__init__(groups)
         self.image = pygame.transform.scale(image, (settings.TILE_SIZE, settings.TILE_SIZE))
         self.rect = self.image.get_rect(topleft=position)
         self.hit_box = self.rect
 
         # Create movement variables
-        self.direction = pygame.math.Vector2((0, 1))
+        self.movement_vector = pygame.math.Vector2((0, 1))
         self.speed = speed
         self.min_y_position = self.rect.topleft[1]
         self.max_net_length = settings.TILE_SIZE * net_length
@@ -19,18 +19,16 @@ class SpiderEnemy(CustomDrawSprite):
         # Real position is required to store the real distance, which is then casted to integer
         self.real_y_position = float(self.hit_box.y)
 
+        # Moving obstacles
+        self.moving_obstacle_sprites = moving_obstacle_sprites
+
     def move(self):
         # Calculate real y position
-        self.real_y_position += float(self.direction.y * self.speed)
+        self.real_y_position += float(self.movement_vector.y * self.speed)
 
-        # Cast real position to integer
+        # Cast real position to integer and check the collision
         self.hit_box.y = int(self.real_y_position)
-
-        # Movement characteristic
-        if self.hit_box.y > self.min_y_position + self.max_net_length:
-            self.direction.y = self.direction.y * -1
-        if self.hit_box.y < self.min_y_position:
-            self.direction.y = self.direction.y * -1
+        self.collision()
 
         # Set the movement offset
         self.rect.center = self.hit_box.center
@@ -47,6 +45,22 @@ class SpiderEnemy(CustomDrawSprite):
         # Draw sprite
         offset_position = self.rect.topleft + offset
         game_surface.blit(self.image, offset_position)
+
+    def collision(self):
+        # Moving obstacle
+        for sprite in self.moving_obstacle_sprites:
+            if sprite.hit_box.colliderect(self.hit_box):
+                if self.movement_vector.y > 0:
+                    self.hit_box.bottom = sprite.hit_box.top
+                if self.movement_vector.y < 0:
+                    self.hit_box.top = sprite.hit_box.bottom
+                self.movement_vector.y = self.movement_vector.y * -1
+                self.real_y_position = float(self.hit_box.y)
+            # Movement characteristic
+            elif self.hit_box.y > self.min_y_position + self.max_net_length:
+                self.movement_vector.y = self.movement_vector.y * -1
+            elif self.hit_box.y < self.min_y_position:
+                self.movement_vector.y = self.movement_vector.y * -1
 
 
 
