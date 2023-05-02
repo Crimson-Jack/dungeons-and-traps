a7b92034f2ba7a066babe14527a39e8cb48b14e3
crimsonking <jacek.jarczak@gmail.com>
2023-05-02 23:03:00 +0200

    feat(game): Diamonds graphics added.

--- data/tsx/tilemap.tsx
+++ data/tsx/tilemap.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="tilemap_packed" tilewidth="16" tileheight="16" tilecount="144" columns="12">
- <image source="../../img/tilemap_packed.png" width="192" height="192"/>
+<tileset version="1.9" tiledversion="1.9.2" name="tilemap_packed" tilewidth="16" tileheight="16" tilecount="168" columns="12">
+ <image source="../../img/tilemap_packed.png" width="192" height="224"/>
  <tile id="134">
   <animation>
    <frame tileid="134" duration="100"/>
