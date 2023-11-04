d826fff1af0560d71403035586fc52d3ddaa7f0e
Crimson Jack <crmsnjck@gmail.com>
2023-11-04 18:05:07 +0100

    chore(game): Refactoring. Cleaning up.

--- data/tsx/ghost-enemy.tsx
+++ data/tsx/ghost-enemy.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="ghost-enemy" tilewidth="16" tileheight="16" tilecount="16" columns="4">
- <image source="../../img/ghost-enemy.png" width="64" height="64"/>
+<tileset version="1.10" tiledversion="1.10.2" name="ghost-enemy" tilewidth="16" tileheight="16" tilecount="12" columns="4">
+ <image source="../../img/ghost-enemy.png" width="64" height="48"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="700"/>
--- data/tsx/monster-enemy-blue.tsx
+++ data/tsx/monster-enemy-blue.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="monster-enemy-blue" tilewidth="16" tileheight="16" tilecount="16" columns="4">
- <image source="../../img/monster-enemy-blue.png" width="64" height="64"/>
+<tileset version="1.10" tiledversion="1.10.2" name="monster-enemy-blue" tilewidth="16" tileheight="16" tilecount="8" columns="4">
+ <image source="../../img/monster-enemy-blue.png" width="64" height="32"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="100"/>
