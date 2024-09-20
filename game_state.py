2146dfafba61b477b1426725932912fdeb2f8a53
Crimson Jack <crmsnjck@gmail.com>
2024-09-20 22:27:39 +0200

    feat(teleport): Graphics.

--- game_state.py
+++ game_state.py
@@ -14,7 +14,7 @@ class GameState:
             # 's01_level_03.tmx',
             # 's01_level_04.tmx',
             # 's01_level_05.tmx',
-            # 's01_level_06.tmx',
+            's01_level_06.tmx',
             'basic.tmx',
         ]
 
